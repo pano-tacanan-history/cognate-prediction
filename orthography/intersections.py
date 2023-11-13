b0701d328a247731a0160680e02d49c668ce1552
Frederic Blum <frederic_blum@eva.mpg.de>
2023-11-13 10:41:14 +0100

    update

--- orthography/intersections.py
+++ orthography/intersections.py
@@ -1,4 +1,4 @@
-"""This module computes the intersections of conceptlists"""
+"""This module computes the intersecs of conceptlists"""
 import csv
 from csvw.dsv import UnicodeDictReader
 from collections import defaultdict
@@ -36,7 +36,7 @@ def read_wl(path):
     return wordlist
 
 
-def intersec(cl1, cl2):
+def compute_intersec(cl1, cl2):
     intersecting = defaultdict()
     missing_from_cl1 = defaultdict()
 
@@ -56,49 +56,49 @@ swadesh = read_cl(PATH + "Swadesh-1952-200.tsv")
 conceptlists = {
     "oliveira": read_cl(PATH + "Oliveira-2014-517.tsv"),
     "girard": read_cl('../cldf-data/girardprototakanan/etc/proto_concepts.tsv'),
-    "blum": read_cl('../cldf-data/blumpanotacana/etc/concepts.tsv'),
     "valenzuela": read_cl('../cldf-data/valenzuelazariquieypanotakana/etc/concepts.tsv')
 }
 
 predictions = Wordlist("predictions/full_predictions.tsv")
 oliveira = read_wl("../cldf-data/oliveiraprotopanoan/cldf/cldf-metadata.json")
 valenzuela = read_wl("../cldf-data/valenzuelazariquieypanotakana/cldf/cldf-metadata.json")
-blum = read_wl("../cldf-data/blumpanotacana/cldf/cldf-metadata.json")
 
 
-def extract_shared(preds, dataset, name):
+def extract_shared(preds, data, name):
     conceptlist = conceptlists[name]
-    intersection, _ = intersec(conceptlist, conceptlists["girard"])
+    intersec, _ = compute_intersec(conceptlist, conceptlists["girard"])
 
     shared_forms = defaultdict()
-    for word in dataset:
-        if dataset[word, "concept"] in intersection and dataset[word, "doculect"] in ["ShipiboConibo", "ShipiboKonibo", "SK"]:
-            form = "".join([x for x in dataset[word, "alignment"] if x not in ["-", " ", "(", ")", "+"]])
-            shared_forms[dataset[word, "concept"]] = form
+    exclude = ["-", " ", "(", ")", "+"]
+    for word in data:
+        if data[word, "concept"] in intersec and data[word, "doculect"] in ["ShipiboKonibo", "SK"]:
+            form = "".join([x for x in data[word, "alignment"] if x not in exclude])
+            shared_forms[data[word, "concept"]] = form
 
-    output_table = [["ID", "Concept", "Concepticon", "ProtoTakana", "ProtoPano_predicted", "Doculect", "Predicted", "Form"]]
+    output_table = [[
+        "ID", "Concept", "Concepticon", "ProtoTakana",
+        "ProtoPano_predicted", "Doculect", "Predicted", "Form"
+        ]]
     for word in preds:
         if preds[word, "concepticon"] in shared_forms:
             preds[word, "form"] = shared_forms[preds[word, "concepticon"]]
             output_table.append(preds[word])
 
-    # print(output_table)
     out = "predictions/shared_" + name + ".tsv"
-    with open(out, 'w', encoding="utf8", newline='') as f:
-        writer = csv.writer(f, delimiter="\t")
-        writer.writerows(output_table)
+    with open(out, 'w', encoding="utf8", newline='') as file:
+        write_file = csv.writer(file, delimiter="\t")
+        write_file.writerows(output_table)
 
 
 extract_shared(predictions, oliveira, "oliveira")
 extract_shared(predictions, valenzuela, "valenzuela")
-extract_shared(predictions, blum, "blum")
 
-no_intersection = []
+no_intersec = []
 for item in predictions:
     concept = predictions[item, "concepticon"]
-    if concept not in conceptlists["oliveira"] and concept not in conceptlists["blum"] and concept not in conceptlists["valenzuela"]:
-        no_intersection.append(predictions[item])
+    if concept not in conceptlists["oliveira"] and concept not in conceptlists["valenzuela"]:
+        no_intersec.append(predictions[item])
 
 with open("predictions/eval_predictions.tsv", 'w', encoding="utf8", newline='') as f:
     writer = csv.writer(f, delimiter="\t")
-    writer.writerows(no_intersection)
+    writer.writerows(no_intersec)
--- orthography/predict.py
+++ orthography/predict.py
@@ -1,73 +1,13 @@
 """
-Code to group and ungroup segmens in comparative wordlists.
+Code to predict forms in Shipibo-Konibo based on forms in 
+Proto-Takana, based on sound correspondences proposed by
+Valenzuela & Zariquiey (2023), Girard (1971), and Oliveira (2014).
 """
-from lingpy import Wordlist
+from collections import defaultdict
 import csv
 from csvw.dsv import UnicodeDictReader
-from unicodedata import normalize
-from collections import defaultdict
-
-
-def profile_sequence(string, segments, maxlen=None):
-    if len(string) == 1:
-        return string
-    max_len = maxlen or max([len(x) for x in segments])
-    
-    # initialize queue
-    queue = [([''], 0, string)]
-
-    out = []
-    while queue:
-        current_sequence, length, rest = queue.pop(0)
-        next_element = rest[0]
-        combined_element = current_sequence[-1] + next_element
-        clen = len(combined_element)
-
-        if len(rest) > 1:
-            if combined_element in segments or not current_sequence[-1] or clen < max_len:
-                queue += [[
-                    current_sequence[:-1]+[combined_element],
-                    length,
-                    rest[1:]
-                    ]]
-            if current_sequence[-1] in segments:
-                queue += [[
-                    current_sequence + [next_element],
-                    length+1,
-                    rest[1:]
-                    ]]
-        else:
-            seqA = current_sequence[:-1]+[combined_element]
-            seqB = current_sequence + [next_element]
-            
-            if not [x for x in seqA if (x not in segments and len(x) > 1)]:
-                out += [seqA]
-            if not [x for x in seqB if (x not in segments and len(x) > 1)]:
-                out += [seqB]
-    if out:
-        return ' '.join(sorted(out, key=lambda x: len([y for y in x if y[0] !=
-            '<']))[0])
-    return ' '.join(['<{0}>'.format(x) if x in segments else
-        x for x in string])
-
-def get_profile(filename, delimiter="\t", space="_"):
-    profile = {}
-    with UnicodeDictReader(filename, delimiter=delimiter) as reader:
-        for row in reader:
-            profile[row["Grapheme"]] = row
-    profile[space] = {k: "NULL" for k in row}
-    return profile
-
-
-def segment(sequence, profile, replace="IPA", space="_"):
-    segments = set(profile)
-    segmented = profile_sequence(normalize("NFC", space.join(sequence)), segments).split(" ")
-    out = []
-    for seg in segmented:
-        rep = profile.get(seg, {replace: "«"+seg+"»"})[replace]
-        if rep != "NULL":
-            out += [rep]
-    return out
+from lingpy import Wordlist
+from grsn import SoundGrouper
 
 
 PATH = '../cldf-data/girardprototakanan/cldf/parameters.csv'
@@ -78,30 +18,47 @@ with UnicodeDictReader(PATH, delimiter=',') as reader:
 
 
 wl = Wordlist("prototakana.tsv")
-prf = get_profile("profiles/takana_to_pano.tsv")
+prf = SoundGrouper.from_file("profiles/takana_to_pano.tsv", delimiter="\t")
 
-i = 1
+i = 0
 D = {0: ["concept", "concepticon", "prototakana", "protopano", "doculect", "predicted", "form"]}
-for idx, tokens_ in wl.iter_rows("tokens"):
+for idx, tokens in wl.iter_rows("tokens"):
     if wl[idx, "doculect"] == "ProtoTakana":
-        PRED =  "".join(segment(tokens_, prf))
-        D[i] = [wl[idx, "concept"], concepts[wl[idx, "concept"]], wl[idx, "form"], PRED, "Shipibo", "", ""]
         i += 1
+        PRED =  "".join(prf("".join(tokens), column="IPA"))
+        D[i] = [
+            wl[idx, "concept"],
+            concepts[wl[idx, "concept"]],
+            wl[idx, "form"],
+            PRED,
+            "Shipibo",
+            "",
+            ""
+            ]
 
 # Convert from Proto-Pano prediction to Shipibo
 wl = Wordlist(D)
-prf = get_profile("profiles/pano_to_shipibo.tsv")
-final_pred = [[
-    "ID", "Concept", "Concepticon", "ProtoTakana", "ProtoPano_predicted", "Doculect", "Predicted", "Form"
+prf = SoundGrouper.from_file("profiles/pano_to_shipibo.tsv", delimiter="\t")
+final = [[
+    "ID", "Concept", "Concepticon", "ProtoTakana",
+    "ProtoPano_predicted", "Doculect", "Predicted", "Form"
 ]]
 
-i = 1
-for idx, tokens_ in wl.iter_rows("protopano"):
-    PRED =  "".join(segment(tokens_, prf))
+i = 0
+for idx, tokens in wl.iter_rows("protopano"):
+    PRED =  "".join(prf("".join(tokens), column="IPA"))
     if wl[idx, "concepticon"] != "":
-        final_pred.append([idx, wl[idx, "concept"], wl[idx, "concepticon"], wl[idx, "prototakana"], wl[idx, "protopano"], "Shipibo", PRED, ""])
         i += 1
+        final.append([
+            idx, wl[idx, "concept"],
+            wl[idx, "concepticon"],
+            wl[idx, "prototakana"],
+            wl[idx, "protopano"],
+            "Shipibo",
+            "".join(PRED),
+            ""
+            ])
 
 with open("predictions/full_predictions.tsv", 'w', encoding="utf8", newline='') as f:
     writer = csv.writer(f, delimiter="\t")
-    writer.writerows(final_pred)
+    writer.writerows(final)
